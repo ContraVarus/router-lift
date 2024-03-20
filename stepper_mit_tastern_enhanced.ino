d8ffab936a7ba2de29cf4262c0d060a1d7dc5075
ContraVarus <contravarus@weissalles.de>
2024-03-20 07:11:06 +0100

    Display function added as function and small enhancements

--- stepper_mit_tastern_enhanced.ino
+++ stepper_mit_tastern_enhanced.ino
@@ -19,17 +19,21 @@
 #define BUTTON_PIN_2 8
 #define BUTTON_PIN_3 4
 
+// Definition für Endstopps
+#define BUTTON_PIN_4 A1
+#define BUTTON_PIN_5 12
+
 //Definition für OLED Display
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
 #define OLED_RESET -1
 #define SCREEN_ADDRESS 0x3C
 
+// Anzahl der Mikroschritte pro Umdrehung
+#define MICROSTEPS 3200
+
 //Display Objekt erstellen
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
-
-// Anzahl der Mikroschritte pro Umdrehung
-#define MICROSTEPS 1600
 
 // Schrittmotor-Objekt erstellen
 Stepper motor(MICROSTEPS, DIR_PIN, PUL_PIN);
@@ -37,9 +41,11 @@ Stepper motor(MICROSTEPS, DIR_PIN, PUL_PIN);
 // Variable zur Speicherung des vorherigen Zustands der Taster
 int prevButtonState1 = HIGH;
 int prevButtonState2 = HIGH;
-int prevButtonState3 = HIGH;
+int prevButtonState3 = LOW;
+int prevButtonState4 = HIGH;
+int prevButtonState5 = HIGH;
 int stepSize = 0;
-bool SpeedMode = true;
+bool SpeedMode = false;
 unsigned long Startzeit;
 unsigned long GesicherteZeit = 0;
 
@@ -51,59 +57,73 @@ void setup() {
   pinMode(BUTTON_PIN_1, INPUT_PULLUP);
   pinMode(BUTTON_PIN_2, INPUT_PULLUP);
   pinMode(BUTTON_PIN_3, INPUT_PULLUP);
-  
+  pinMode(BUTTON_PIN_4, INPUT_PULLUP);
+  pinMode(BUTTON_PIN_5, INPUT_PULLUP);
+
+  motor.setSpeed(40); //Anfangsgeschwindigkeit nach Programmstart auf langsam festsetzen
+
   if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) { // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
   Serial.println(F("SSD1306 allocation failed"));
   for(;;); // Don't proceed, loop forever
+  display.clearDisplay();
+  display.print("Wullis Woodwork");
+  display.display();
+}
+}
 
+void showDisplay() {
+  display.clearDisplay();
+  display.setCursor(0,20);
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+  display.print("Hoehe: ");
+  display.print(stepSize * 0.01953125, 2);
+  display.println("mm");
+  display.display();
 }
-}
+
+
 void loop() {
   // Aktueller Zustand der Taster
   int buttonState1 = digitalRead(BUTTON_PIN_1);
   int buttonState2 = digitalRead(BUTTON_PIN_2);
   int buttonState3 = digitalRead(BUTTON_PIN_3); //Wert von 1 kommt zurück wenn Button auf HIGH steht
-  
-if (buttonState3 == LOW) {
+  int buttonState4 = digitalRead(BUTTON_PIN_4); //Wert von 1 kommt wenn Mikroschalter nicht gedrückt ist, bei Druck kommt Wert 1
+  int buttonState5 = digitalRead(BUTTON_PIN_5);
+
+digitalRead(buttonState4);
+
+  if (buttonState3 == LOW) {
   Startzeit = millis();
-if (Startzeit - GesicherteZeit > 50)
+  if (Startzeit - GesicherteZeit > 50)
 {
   SpeedMode = !SpeedMode;
 }
  GesicherteZeit = Startzeit;
  digitalWrite(buttonState3, SpeedMode);
- Serial.println(SpeedMode);
-
+ 
 // Setze die Geschwindigkeit des Motors in Umdrehungen pro Minute (RPM)
-if (SpeedMode == 1) {
-    motor.setSpeed(160); // Hier kannst du die Geschwindigkeit ändern, wie du möchtest
+  if (SpeedMode == 1) {
+  motor.setSpeed(800); // Hier kannst du die Geschwindigkeit ändern, wie du möchtest
+ }
+  else {
+  motor.setSpeed(100);
+  }
 }
-else if (SpeedMode == 0) {
-    motor.setSpeed(40);
-}
-}
-
   // Wenn der erste Taster gedrückt wird und der vorherige Zustand nicht gedrückt war
   if (buttonState1 == LOW && prevButtonState1 == HIGH) {
     // Solange der erste Taster gedrückt ist
     while (digitalRead(BUTTON_PIN_1) == LOW) {
-      // Drehung um 100 Mikrostufen im Uhrzeigersinn
-      motor.step(100);
+      // Drehung um 200 Mikrostufen im Uhrzeigersinn
+      motor.step(200);
       // Inkrementiere die Schrittgröße
-      stepSize++;
+      ++stepSize;
       // Kurze Verzögerung für eine angemessene Geschwindigkeit
       delay(10);
         // Ausgabe der Schrittgröße in Millimeter über die serielle Konsole
      Serial.print("Step size in millimeters: ");
      Serial.println(stepSize * 0.01953125, 2);
-     display.clearDisplay();
-     display.setCursor(0,20);
-     display.setTextSize(1);
-     display.setTextColor(SSD1306_WHITE);
-     display.print("Hoehe: ");
-     display.print(stepSize * 0.01953125, 2);
-     display.println("mm");
-     display.display();
+    showDisplay();
     }
   }
 
@@ -111,28 +131,19 @@ else if (SpeedMode == 0) {
   if (buttonState2 == LOW && prevButtonState2 == HIGH) {
     // Solange der zweite Taster gedrückt ist
     while (digitalRead(BUTTON_PIN_2) == LOW) {
-      // Drehung um 100 Mikrostufen gegen den Uhrzeigersinn
-      motor.step(-100);
+      // Drehung um 200 Mikrostufen gegen den Uhrzeigersinn
+      motor.step(-200);
       // Dekrementiere die Schrittgröße
-      stepSize--;
+      --stepSize;
       // Kurze Verzögerung für eine angemessene Geschwindigkeit
       delay(10);
      // Ausgabe der Schrittgröße in Millimeter über die serielle Konsole
      Serial.print("Step size in millimeters: ");
      Serial.println(stepSize * 0.01953125, 2);
-     Serial.print("Step size in millimeters: ");
-     Serial.println(stepSize * 0.01953125, 2);
-     display.clearDisplay();
-     display.setCursor(0,20);
-     display.setTextSize(1);
-     display.setTextColor(SSD1306_WHITE);
-     display.print("Hoehe: ");
-     display.print(stepSize * 0.01953125, 2);
-     display.println("mm");
-     display.display();
+    showDisplay();
     }
   }
- 
+
   // Speichere den aktuellen Zustand der Taster für den nächsten Durchlauf
   prevButtonState1 = buttonState1;
   prevButtonState2 = buttonState2;
