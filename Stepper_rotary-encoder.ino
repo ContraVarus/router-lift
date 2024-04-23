fbb0c275e6c01c75b67fa520b21bbc4f62dc18c8
ContraVarus <contravarus@weissalles.de>
2024-04-23 08:32:51 +0200

    Update Stepper_rotary-encoder.ino

--- Stepper_rotary-encoder.ino
+++ Stepper_rotary-encoder.ino
@@ -29,9 +29,11 @@ const int dirPin = 7;
 const int stepPin = 8;
 int lastpos = 0;
 signed long position = 0;
+signed char pos = 0;
+bool motorrun = 0;
 
 AccelStepper stepper(AccelStepper::DRIVER, stepPin, dirPin);
-EncoderStepCounter encoder(ENCODER_PIN1, ENCODER_PIN2, HALF_STEP);
+EncoderStepCounter encoder(ENCODER_PIN1, ENCODER_PIN2, FULL_STEP);
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS); 
 
 void setup() {
@@ -42,15 +44,16 @@ void setup() {
   attachInterrupt(ENCODER_INT1, interrupt, CHANGE);
   attachInterrupt(ENCODER_INT2, interrupt, CHANGE);
 
-  stepper.setMaxSpeed(500);
-  stepper.setAcceleration(25.0);
+  stepper.setMaxSpeed(100);
+  stepper.setAcceleration(50.0);
 
   if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) { 
     Serial.println(F("SSD1306 allocation failed"));
     for (;;);
-    display.clearDisplay();
-    display.display();
   }
+
+  display.clearDisplay();
+  display.display();
 }
 
 void interrupt() {
@@ -64,24 +67,15 @@ void showDisplay() {
   display.setTextColor(SSD1306_WHITE);
   display.println("Height: ");
   display.setCursor(10, 40);
-  display.print(position * 0.025, 1);
+  display.print(pos * 0.025, 1);
   display.print(" mm");
   display.display();
 }
 
 void loop() {
-  signed char pos = encoder.getPosition();
-  if (pos != 0) {
-    position += pos;
-    Serial.println(pos);
-    encoder.reset();
-    Serial.println(position);
-  }
-  if (position != lastpos) {
-    stepper.moveTo(position);
-    stepper.run();
-    Serial.println(position);
-    showDisplay();
-  }
-
+  pos = encoder.getPosition();
+  position += pos;
+  encoder.reset();
+  stepper.moveTo(position);
+  stepper.run();
 }
