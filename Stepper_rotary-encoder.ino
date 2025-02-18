4edacb6979177e72f3a11514d5ea7ed381ded4d1
ContraVarus <contravarus@weissalles.de>
2025-02-18 18:41:31 +0100

    Update Stepper_rotary-encoder.ino

--- Stepper_rotary-encoder.ino
+++ Stepper_rotary-encoder.ino
@@ -1,13 +1,9 @@
+#include <AccelStepper.h>
+
 #include <Adafruit_SSD1306.h>
 #include <Adafruit_GFX.h>
 #include <gfxfont.h>
 #include <splash.h>
-
-#include <SPI.h>
-#include <Wire.h>
-
-#include <AccelStepper.h>
-#include <MultiStepper.h>
 
 #include <EncoderStepCounter.h>
 
@@ -18,64 +14,70 @@
 
 #define SCREEN_WIDTH 128 //OLED Definition
 #define SCREEN_HEIGHT 64
-#define OLED_RESET  11
-#define OLED_MOSI 12  //Pin SDA on OLED
-#define OLED_CLK  13  // Pin SCL on OLED
-#define OLED_DC 10  
-#define OLED_CS 9
+#define OLED_RESET  4
+#define OLED_MOSI 5  //Pin SDA on OLED
+#define OLED_CLK  6  // Pin SCL on OLED
+#define OLED_DC 7  
+#define OLED_CS 8
 #define SCREEN_ADDRESS 0x3D
 
-const int dirPin = 7;
-const int stepPin = 8;
+const int dirPin = 9;
+const int stepPin = 10;
 int lastpos = 0;
 signed long position = 0;
 signed char pos = 0;
-bool motorrun = 0;
+unsigned long lastMoveTime = 0;
+unsigned long TimeOut = 2000;
 
-AccelStepper stepper(AccelStepper::DRIVER, stepPin, dirPin);
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS); //Display Objekt erstellen
 EncoderStepCounter encoder(ENCODER_PIN1, ENCODER_PIN2, FULL_STEP);
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS); 
+AccelStepper stepper(AccelStepper::DRIVER, stepPin, dirPin);
 
 void setup() {
   Serial.begin(9600);
- 
   encoder.begin();
- 
   attachInterrupt(ENCODER_INT1, interrupt, CHANGE);
   attachInterrupt(ENCODER_INT2, interrupt, CHANGE);
 
-  stepper.setMaxSpeed(100);
+  stepper.setMaxSpeed(400);
   stepper.setAcceleration(50.0);
 
-  if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) { 
+  if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) { // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
     Serial.println(F("SSD1306 allocation failed"));
-    for (;;);
+    for (;;); // Don't proceed, loop forever
   }
 
+  display.clearDisplay(); //Nach Start mit leeren Display starten
+  display.display();
+}
+
+void showDisplay() {
   display.clearDisplay();
+  display.setCursor(10, 0);
+  display.setTextSize(2);
+  display.setTextColor(SSD1306_WHITE);
+  display.print("H");
+  display.print(char(0x94));
+  display.print("he:");
+  display.setCursor(10, 20);
+  display.print(position * 0.0125, 1);
+  display.print(" mm");
+  // display.setCursor(10, 40);
+  // display.print("moved: ");
+  // display.print(moved);
   display.display();
 }
 
 void interrupt() {
   encoder.tick();
-}
-
-void showDisplay() {
-  display.clearDisplay();
-  display.setCursor(10, 10);
-  display.setTextSize(2);
-  display.setTextColor(SSD1306_WHITE);
-  display.println("Height: ");
-  display.setCursor(10, 40);
-  display.print(pos * 0.025, 1);
-  display.print(" mm");
-  display.display();
+  lastMoveTime = millis();
 }
 
 void loop() {
   pos = encoder.getPosition();
   position += pos;
   encoder.reset();
-  stepper.moveTo(position);
-  stepper.run();
+  if (millis() - lastMoveTime >= TimeOut){
+    showDisplay();
+ } else {display.clearDisplay();}
 }
